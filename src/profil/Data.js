42d87bfd7f967452c15c6c810b327331137570b0
maher-moussi <mahermoussi777@gmail.com>
2021-03-31 13:16:19 +0200

    frist commit

---
+++ src/profil/Data.js
@@ -0,0 +1,8 @@
+const Data =[{
+    image: "https://www.who2.com/wp-content/uploads/2015/10/denzelwashington-43-272x363.jpg",
+    name:"Denzel Washington", 
+    bio : "December 28, 1954, Mount Vernon, New York",
+    profession : "Acteur"
+},
+]
+export default Data;
